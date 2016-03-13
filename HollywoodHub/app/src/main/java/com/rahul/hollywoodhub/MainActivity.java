a5b926f75392f263febdf025431b2aaa802a7252
rahul pandey <rahul.pandey@8finatics.com>
2016-03-13 16:28:32 +0530

    Added Projects

---
+++ HollywoodHub/app/src/main/java/com/rahul/hollywoodhub/MainActivity.java
@@ -0,0 +1,287 @@
+package com.rahul.hollywoodhub;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,android.support.v7.widget.SearchView.OnQueryTextListener {
+    private Toolbar toolbar;
+    private static ImageView header;
+    private CollapsingToolbarLayout collapsingToolbar;
+    private TabPagerAdapter mTabPagerAdapter;
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+    public String[] genre;
+    public Spinner spinner;
+    TextView genreIVButton;
+    private String searchQuery="";
+    static boolean forMoviePage = true;
+    String tvSeriesText = "";
+    Snackbar snack;
+    boolean doubleBackToExitPressedOnce = false;
+    static String GENRE_LINK="http://www.watchfree.to/?genre=", SEARCH_SECTION_TV="&search_section=2", SEARCH_SECTION_MOVIE="&search_section=1";
+    static String DEFAULT_LINK="http://www.watchfree.to/?sort=";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar= (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+        collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        genre = getResources().getStringArray(R.array.genre);
+        mViewPager = (ViewPager) findViewById(R.id.view_pager);
+        mTabLayout = (TabLayout) findViewById(R.id.detail_tabs);
+        genreIVButton = (TextView) findViewById(R.id.genre_iv);
+        header = (ImageView) findViewById(R.id.backdrop);
+        genreIVButton.setTranslationY(-25f);
+//        mTabLayout.setTranslationY(-20f);
+        spinner = (Spinner) findViewById(R.id.spinner_nav);
+        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.genre, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (genre[position].equalsIgnoreCase("all")) {
+                    collapsingToolbar.setTitle("Hollywood");
+                    getSupportActionBar().setTitle("Hollywood");
+                }
+                else{
+                    collapsingToolbar.setTitle(genre[position]);
+                    getSupportActionBar().setTitle(genre[position]);
+                }
+                searchQuery = "";
+                if (position == 0)
+                    setViewPagerAdapter(DEFAULT_LINK);
+                else
+                    setViewPagerAdapter(GENRE_LINK + spinner.getSelectedItem().toString() + "&sort=");
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        genreIVButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spinner.performClick();
+            }
+        });
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void setViewPagerAdapter(String link) {
+        mTabPagerAdapter = new TabPagerAdapter(getSupportFragmentManager(), link);
+        mTabLayout.setTabsFromPagerAdapter(mTabPagerAdapter);
+        mViewPager.setAdapter(mTabPagerAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            if (doubleBackToExitPressedOnce) {
+                super.onBackPressed();
+                return;
+            }
+            this.doubleBackToExitPressedOnce = true;
+            snack = Snackbar.make(drawer, "Press back again to exit", Snackbar.LENGTH_LONG)
+                    .setActionTextColor(getResources().getColor(android.R.color.white));
+            ViewGroup group = (ViewGroup) snack.getView();
+            group.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+            snack.show();
+//            Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    doubleBackToExitPressedOnce = false;
+                }
+            }, 2000);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        android.support.v7.widget.SearchView searchView = (android.support.v7.widget.SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+        searchView.setOnQueryTextListener(this);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            Picasso.with(MainActivity.this).load(R.drawable.header_movie).into(header);
+            spinner.setSelection(0);
+            collapsingToolbar.setTitle("Hollywood");
+            searchQuery = "";
+            tvSeriesText = "";
+            forMoviePage = true;
+            setViewPagerAdapter(DEFAULT_LINK);
+        } else if (id == R.id.nav_gallery) {
+            Picasso.with(MainActivity.this).load(R.drawable.header_tvseries).into(header);
+            collapsingToolbar.setTitle("TV Series");
+            spinner.setSelected(false);
+            spinner.setSelection(0);
+            searchQuery = "";
+            tvSeriesText = "&tv=";
+            forMoviePage = false;
+            setViewPagerAdapter(DEFAULT_LINK);
+        }
+//        else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        tvSeriesText = "";
+        if (forMoviePage)
+            searchQuery = query.replace(" ", "+")+SEARCH_SECTION_MOVIE;
+        else
+            searchQuery = query.replace(" ", "+")+SEARCH_SECTION_TV;
+        collapsingToolbar.setTitle(query);
+        getSupportActionBar().setTitle(query);
+        setViewPagerAdapter(DEFAULT_LINK);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
+    }
+
+    class TabPagerAdapter extends FragmentStatePagerAdapter{
+
+        String link;
+        public TabPagerAdapter(FragmentManager fm, String link) {
+            super(fm);
+            this.link = link;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return RecyclerViewFragment.newInstance(link+"&keyword="+searchQuery+tvSeriesText);
+                case 1:
+                    return RecyclerViewFragment.newInstance(link+"release&keyword="+searchQuery+tvSeriesText);
+                case 2:
+                    return RecyclerViewFragment.newInstance(link+"views&keyword="+searchQuery+tvSeriesText);
+                case 3:
+                    return RecyclerViewFragment.newInstance(link+"alphabet&keyword="+searchQuery+tvSeriesText);
+                default:
+                    return RecyclerViewFragment.newInstance(DEFAULT_LINK);
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position){
+                case 0:
+                    return "Top";
+                case 1:
+                    return "Release";
+                case 2:
+                    return "Popular";
+                case 3:
+                    return "A-Z";
+                default:
+                    return "All";
+            }
+        }
+    }
+    public static ImageView getHeaderImage(){
+        return header;
+    }
+}
---
+++ MyApplication/app/src/main/java/com/rahul/myapplication/ContentViewActivity.java
@@ -0,0 +1,194 @@
+package com.rahul.myapplication;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import com.squareup.picasso.Picasso;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.rahul.myapplication.R.id.content_title;
+
+public class ContentViewActivity extends AppCompatActivity {
+
+    String link,desc,title="Please Wait ...",image_link;
+    Document document;
+    TextView textView, titleTextView;
+    RecyclerView recyclerView;
+    MyAdapter myAdapter;
+    ImageView imageView;
+    List<Information> list = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/Montserrat-Regular.ttf");
+        setContentView(R.layout.activity_content_view);
+        textView= (TextView)findViewById(R.id.desc);
+        titleTextView = (TextView) findViewById(content_title);
+        textView.setTypeface(tf);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Intent intent = getIntent();
+        titleTextView.setText(intent.getStringExtra("title"));
+        titleTextView.setTypeface(tf);
+        link = intent.getStringExtra("next_link");
+        desc = intent.getStringExtra("desc");
+        image_link = intent.getStringExtra("image_link");
+        textView.setText(desc);
+
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        //Toast.makeText(getApplicationContext(), "description  "+desc, Toast.LENGTH_SHORT).show();
+        ((AppCompatActivity) this).getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().set
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerList);
+
+        isNetworkAvailable();
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Open Webpage Link", Snackbar.LENGTH_LONG).setActionTextColor(getResources().getColor(android.R.color.white))
+                        .setAction("OPEN", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+                                startActivity(intent);
+                            }
+                        }).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+//                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    class JsoupAsyncTask extends AsyncTask<String, Void, Boolean> {
+
+        private ProgressDialog dialog;
+        @Override
+        protected void onPreExecute() {
+            recyclerView.setVisibility(View.GONE);
+            dialog = new ProgressDialog(ContentViewActivity.this);
+            dialog.setMessage("Please Wait !");
+            dialog.show();
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            try {
+                document = Jsoup.connect(link).get();
+                Elements elements = document.select("div.postcontent > *");
+                title = elements.get(1).text();
+                if (title.isEmpty())
+                    title = elements.get(2).text();
+                //desc = elements.get(2).text();
+//                image_link = elements.get(0).select("img[src]").attr("src");
+//                if (image_link.isEmpty())
+//                    image_link = elements.get(1).select("img[src]").attr("src");
+                for (Element element : elements.select("blockquote")){
+                    for (Element dwn_lnk : element.select("a[href^=http]")) {
+                        Information information = new Information();
+                        information.title = dwn_lnk.text();
+                        information.link = dwn_lnk.attr("href");
+                        if (!information.link.contains("prochina"))
+                            list.add(information);
+                    }
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean th){
+            ((AppCompatActivity)ContentViewActivity.this).getSupportActionBar().setTitle(title);
+            findViewById(R.id.recyclerList).setVisibility(View.VISIBLE);
+//            titleTextView.setText(title);
+            myAdapter = new MyAdapter(getApplicationContext(), list, false);
+            recyclerView.setAdapter(myAdapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+            if (dialog.isShowing())
+                dialog.dismiss();
+            Picasso.with(ContentViewActivity.this).load(image_link).into(imageView);
+
+        }
+    }
+
+    public void isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        if (activeNetworkInfo != null && activeNetworkInfo.isConnected()){
+            JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+            jsoupAsyncTask.execute();
+        }
+        else showDialogBox();
+    }
+
+    public boolean showDialogBox(){
+        android.app.AlertDialog.Builder dialog = new android.app.AlertDialog.Builder(ContentViewActivity.this);
+        dialog.setTitle("Network Connectivity");
+        dialog.setMessage("No internet connection detected please try again");
+        dialog.setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                isNetworkAvailable();
+            }
+        });
+        dialog.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                System.exit(0);
+            }
+        });
+//        dialog.setIcon(android.R.drawable.ic_dialog_alert);
+        dialog.setCancelable(false);
+        dialog.show();
+        return true;
+    }
+
+}
